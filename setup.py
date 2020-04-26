04264db78cc027ba1b699735dd9b4dfa8e4d4a5d
Arthur Holzner <arthur.holz.91@gmail.com>
2020-04-26 17:52:02 +0200

    more version fixing

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'Django >=2.0',
         'graphene-django>=2.1.5',
-        'graphql-core>=2.1,<3',
+        'graphene>=2.0',
         'PyJWT>=1.5.0',
     ],
     classifiers=[
